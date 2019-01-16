4ba0fdb5616f150854bb0998202c81f084ce6692
N.Zerof <xtahi0nix@gmail.com>
2019-01-16 12:17:35 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+#include "api.h"
+
+int main(int argc, char *argv[])
+{
+	
+
+	printf("WinAPI hasing test\n");
+
+	
+	cSleep(5000);
+
+    system("pause");
+    return 0;
+}
